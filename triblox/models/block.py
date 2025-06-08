a97efc1929bb265c77d7d39fe9da680a15fba1dc
Danilo Sanchi <danilo.sanchi@gmail.com>
2025-06-08 22:57:57 +0200

    WIP

--- triblox/models/block.py
+++ triblox/models/block.py
@@ -20,6 +20,7 @@ from triblox.bowl.Bowl import Bowl
 from triblox.block.BaseHoleVoid import BaseHoleVoid
 from triblox.block.BaseHoleOnEdgesVoid import BaseHoleOnEdgesVoid
 from triblox.mosaic.MosaicBuilder import MosaicBuilder
+from math import radians, cos, sin
 
 import time
 
@@ -50,7 +51,7 @@ top_void = TopVoid(mosaic, hn)
 # base_hole_on_edges_void = BaseHoleOnEdgesVoid(mosaic)
 duct = Duct(
     Tile(1,1).vertices.c,
-    h(hn),
+    h(hn)-5,
     Tile(1,-2).vertices.c,
     h(1)+7/2+0.5,
     7,
@@ -80,11 +81,41 @@ spout = Bowl(
 spout_border = Prism(spout_top, 3.5/h(1))
 
 enter = Tile(1, 1).vertices.c
+# washer_void = (
+#     Workplane("XY")
+# #     .transformed(offset=(enter.x, enter.y, h(hn) - clr -taper_h - stub_h))
+# )
 washer_void = (
     Workplane("XY")
-    .circle(21/2)
-    .extrude(2)
-    .translate((enter.x, enter.y, h(hn) - clr -taper_h - stub_h - 2))
+    .circle(25.5/2)
+    .extrude(-5)
+)
+
+# for angle in [45, 135, -45, -135]:
+#     r = 18/2
+#     rad = radians(angle)
+#     x = r * cos(rad)
+#     y = r * sin(rad)
+#     washer_void = (
+#         washer_void
+#         .union(
+#             Workplane("XY")
+#             .center(x, y)
+#             .circle(3/2)
+#             .extrude(-11.40-2 +3/2)
+#         )
+#         .union(
+#             Workplane("XY")
+#             .transformed(offset=(0, 0, -11.40-2+3/2))
+#             .center(x, y)
+#             .circle(3/2)
+#             .extrude(-3/2, taper=45)
+#         )
+#     )
+
+washer_void = (
+    washer_void
+    .translate((enter.x, enter.y, h(hn) - clr - taper_h - stub_h))
 )
 
 
@@ -93,7 +124,7 @@ washer_void = (
 
 result = (
     Workplane("XY")
-#     .union(duct.get())
+    .union(duct.get())
     .union(base.get())
     .union(prism.get())
     .cut(top_void.get())
